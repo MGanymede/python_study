9dfa709ee2676a4a75e68ca5ef61b2eb3e9e131a
jesse <xiang19890319@gmail.com>
2019-03-20 22:05:39 +0800

    add rent functionality

--- Tornado_study/1server.py
+++ Tornado_study/1server.py
@@ -1,15 +1,21 @@
+from abc import ABC
+
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+
+class MainHandler(tornado.web.RequestHandler, ABC):
     def get(self):
-       self.write("hello world")
+        self.write("hello world")
+
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler)
     ])
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
--- Tornado_study/2template.py
+++ Tornado_study/2template.py
@@ -1,15 +1,27 @@
+from abc import ABC
+
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+
+class PublicRentHandler(tornado.web.RequestHandler, ABC):
     def get(self):
-       self.render("index.html", title="jesse")
+        self.render("rent.html", title="公共租赁房屋发布")
+
+
+class MainHandler(tornado.web.RequestHandler, ABC):
+    def get(self):
+        self.render("index.html", title="瑞祥爬虫系统")
+
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler)
+        (r"/rent", PublicRentHandler)
     ])
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
